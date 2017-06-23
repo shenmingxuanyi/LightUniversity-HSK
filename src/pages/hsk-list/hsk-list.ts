1f42e8a48c5e2702cb63282733f8803a3ff05015
赵俊明 <sm.xy@live.com>
2017-06-23 23:47:17 +0800

    commit

--- src/pages/hsk-list/hsk-list.ts
+++ src/pages/hsk-list/hsk-list.ts
@@ -2,12 +2,6 @@ import {Component} from '@angular/core';
 import {IonicPage, NavController, NavParams} from 'ionic-angular';
 import {TestProvider} from "../../providers/test/test";
 
-/**
- * Generated class for the HskListPage page.
- *
- * See http://ionicframework.com/docs/components/#navigation for more info
- * on Ionic pages and navigation.
- */
 @IonicPage()
 @Component({
     selector: 'page-hsk-list',
@@ -23,11 +17,7 @@ export class HskListPage {
 
         console.log(this.testProvider.getData())
 
-
     }
 
-    appointmentHsk($event) {
-        this.navCtrl.push("AppointmentHskPage");
-    }
 
 }
